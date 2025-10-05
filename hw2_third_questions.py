be826e2635e8c5e54b75bcdb07c17b0d60fc72ca
DmitryKuptsov <dkupcov@nes.ru>
2025-10-05 13:35:07 +0200

    third question

---
+++ hw2_third_questions.py
@@ -0,0 +1,27 @@
+def total_registered_cases(data, country):
+    if data.get(country) is None:
+        return 0
+    arr_cases = data[country]
+    return sum(arr_cases)
+
+total_registered_cases({'Spain': [4, 8, 2, 0, 1], 'France': [2, 3, 6], 'Italy': [6, 8, 1, 7]}, 'Italy')
+
+
+def total_registered_cases_per_country(data):
+    dict_total_cases = {}
+    for country in data.keys():
+        total = total_registered_cases(data, country)
+        dict_total_cases[country] = total
+    return dict_total_cases
+
+total_registered_cases_per_country({'Spain': [4, 8, 2, 0, 1], 'France': [2, 3, 6], 'Italy': [6, 8, 1, 7]})
+
+
+def country_with_most_cases(data):
+    dict_total_cases = total_registered_cases_per_country(data)
+    max_cnt = 0
+    for country, cnt in dict_total_cases.items():
+        if cnt > max_cnt:
+            max_cnt = cnt
+            max_country = country
+    return max_country
