b499e7d1c6ed2fef7984686d628849c177814e38
sofapirogova <sofia.pirogova@bse.eu>
2025-10-07 21:20:59 +0300

    adding answers for first questions

---
+++ hw2_first_questions.py
@@ -0,0 +1,13 @@
+def triple(x):
+  return x*3
+
+
+def subtract(a, b):
+    return a-b
+
+
+def dictionary_maker(two_tuples):
+    dictionary = {}
+    for key, value in two_tuples:
+        dictionary[key] = value
+    return dictionary
